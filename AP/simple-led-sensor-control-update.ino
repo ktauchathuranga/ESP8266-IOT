300a9b0396b1d4eb6e4dd083cf1a57fb7dbb0f6f
Ashen Chathuranga <79928743+ktauchathuranga@users.noreply.github.com>
2023-08-15 23:24:04 +0530

    Update simple-led-sensor-control-update.ino

--- AP/simple-led-sensor-control-update.ino
+++ AP/simple-led-sensor-control-update.ino
@@ -1,6 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
+ESP8266WebServer server(80);
+
 const char* ssid = "ESP8266_AP";
 const char* password = "password123";
 
@@ -8,8 +10,6 @@ const int ledPin = 2; // Pin for the LED
 const int sensorPin = A0; // Analog pin for the sensor
 
 boolean ledState = false;
-
-ESP8266WebServer server(80);
 
 void setup() {
   Serial.begin(115200);
