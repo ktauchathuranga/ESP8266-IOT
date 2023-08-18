a1dfaaece65d5b506d121d10eeb9d4796c3942a5
Ashen Chathuranga <79928743+ktauchathuranga@users.noreply.github.com>
2023-08-18 15:37:48 +0530

    Update no-param.ino

--- AP/WITHOUT-WEBPAGE/no-param.ino
+++ AP/WITHOUT-WEBPAGE/no-param.ino
@@ -9,15 +9,23 @@ const char* password = "password123";
 const int ledPin = 2; // Pin for the LED
 boolean ledState = false;
 
+IPAddress localIP(192, 168, 1, 1);  // Set your desired IP address
+IPAddress gateway(192, 168, 1, 1); // Set your gateway IP address
+IPAddress subnet(255, 255, 255, 0); // Set your subnet mask
+
 void setup() {
   Serial.begin(115200);
+  delay(10);
 
   pinMode(ledPin, OUTPUT);
-
+  
+  // Connect to WiFi network
+  WiFi.softAPConfig(localIP, gateway, subnet);
   WiFi.softAP(ssid, password);
-  IPAddress myIP = WiFi.softAPIP();
+
+  Serial.println();
   Serial.print("AP IP address: ");
-  Serial.println(myIP);
+  Serial.println(WiFi.softAPIP());
 
   server.on("/toggle", HTTP_POST, [](){
     digitalWrite(ledPin, !ledState); // Toggle LED state
